809ddc50062d9210e1afb3237a72c9fcb660c2ea
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-11-26 13:36:58 +0600

    Add website logos to the project, add opensans font to index.css, add the necessary custom styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xsm": "360px",
+        xsm: "480px",
+        "2md": "850px",
+        "3xl": "1700px",
+      },
+      spacing: {
+        sectionGapLg: "11rem",
+        sectionGapMd: "9rem",
+        sectionGapSm: "5rem",
+        elementGapSm: "1.4rem",
+        elementGapMd: "3rem",
+      },
+      colors: {
+        primary: "#00A3B4",
+        primaryLight: "#33b5c3",
+        primaryLightest: "#b3e3e9",
+        textPrimary: "#1C1B1B",
+        textMediumLight: "#1c1b1bcc",
+        textLight: "#1c1b1b99",
+        lightGray: "#f5f5f5",
+      },
+      fontFamily: {
+        default: "'Open Sans', sans-serif;",
+      },
+      borderRadius: {
+        default: "5px",
+        defaultLg: "10px",
+      },
+    },
   },
   plugins: [],
 };
